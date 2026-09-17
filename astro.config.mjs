cf646c8dc4185d5d3091614a96a1c308f5cffb8c
Tilman Vogel <tilman.vogel@web.de>
2026-09-17 23:42:38 +0200

    add pianoroll bug reproducer

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
         items: [
           // Each item here is one entry in the navigation menu.
           { label: 'Example Guide', slug: 'guides/example' },
+          { label: 'Pianoroll bug', slug: 'guides/bug' },
         ],
       },
       {
